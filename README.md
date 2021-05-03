3e14c9f92be2aaf0603715346967f5b6b37d7e21
Boldy97 <robin.de.bruin@hotmail.com>
2021-05-03 20:29:17 +0200

    more features

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # workout
 
 A simple application that tracks reps, and assigns points if someone else did not complete a rep in an certain interval.
+Please do not judge the code quality. Thank you.
 
 # Usage
 
 There has to be a `data` folder in the root directory of this project.
 
-You can specify a custom interval (in minutes) by using a flag. It defaults to 60
-
 `$ npm install`
 
+`$ node index.js`
 `$ node index.js --interval 10`
+
+# Flags
+
+`port` : default 3000 : the port
+`interval` : default 60 : how long until an extra point is assigned (in minutes)
+`delay` : default 15 : how long the same person has to wait to get another point for consecutive reps
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const binarySearch = require('binarysearch');
 const fs = require('fs').promises;
 const argv = require('minimist')(process.argv.slice(2));
 
-const PORT = 3000;
 const DATA_PATH = 'data/data.json';
+const PORT = argv.port || 3000;
 const INTERVAL = 1000 * 60 * (argv.interval || 60);
+const DELAY = 1000 * 60 * (argv.delay || 15);
 let DATA = {
   actions: [],
   points: {}
@@ -39,12 +40,8 @@ function setupWebserver() {
   app.use(express.static('public'));
 
   app.post('/api/action', (req, res) => {
-    const action = req.body;
-    action.time = Date.now();
-    action.timeVictory = action.time + INTERVAL;
-    DATA.actions.push(action);
-    if(!DATA.points[action.name]) DATA.points[action.name] = 0;
-    saveData();
+    let action = req.body;
+    action = addAction(action);
     res.send(action);
   });
 
@@ -67,15 +64,36 @@ function setupWebserver() {
   });
 }
 
+function addAction(action) {
+  action.time = Date.now();
+  action.timeVictory = action.time + INTERVAL;
+  action.timeDelay = action.time + DELAY;
+  assignPointForNewAction(action);
+  DATA.actions.push(action);
+  saveData();
+  return action;
+}
+
+function assignPointForNewAction(action) {
+  if(!DATA.points[action.name]) DATA.points[action.name] = 0;
+  const previousAction = DATA.actions[DATA.actions.length - 1];
+  if(previousAction && previousAction.name === action.name && Date.now() < previousAction.timeDelay) return;
+  givePoint(action.name);
+}
+
 function updatePoints() {
   const action = DATA.actions[DATA.actions.length - 1];
   if(!action) return;
   if(action.assignedPoint) return;
   if(Date.now() < action.timeVictory) return;
-  console.log('Giving point to', action.name, '!');
-  DATA.points[action.name]++;
+  givePoint(action.name);
   action.assignedPoint = true;
   saveData();
+}
+
+function givePoint(name) {
+  console.log('Giving point to', name, '!');
+  DATA.points[name]++;
 }
 
 start();
